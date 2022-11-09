d7a62c9f47f9f2233c167c2fffc81645b8183519
Faruk Ahmed <farukahmed@pop-os.localdomain>
2022-11-09 10:59:16 +0600

    hello today

--- DOM/index.js
+++ DOM/index.js
@@ -25,3 +25,55 @@ document.getElementsByClassName('h1')[0].innerHTML = 'This My second Heading'
 document.getElementsByClassName('h1')[1].innerHTML = 'This My first Heading'
 
 */
+/*
+// query selector
+document.querySelector('.h1c').innerHTML = 'This My second'
+document.querySelector('#h1id').innerHTML = 'This My first'
+document.querySelector('h1').innerHTML = 'This My 3rd'
+it's like css selector
+*/
+
+// Event Handler to onclick event
+
+/* onclick alert system 
+
+
+function myMsg1() {
+    alert("hello buddy 1")
+}
+function myMsg2() {
+    alert("hello buddy 2")
+}
+function myMsg3() {
+    alert("hello buddy 3")
+}
+function myMsg4() {
+    alert("hello buddy 4")
+}
+*/
+/*
+// show text on document after click 
+var myPid = document.querySelector('#pId');
+
+function myMsg1() {
+    myPid.innerHTML = 'You clicked button 1';
+}
+function myMsg2() {
+    myPid.innerHTML = 'You clicked button 2';
+}
+*/
+
+/*
+//show img on document after click 
+
+var myPid = document.querySelector('#myImg');
+function myPic1() {
+    myPid.src = '/DOM/img/clcik-1.jpg';
+}
+function myPic2() {
+    myPid.src = '/DOM/img/click-2.jpg';
+}
+function myPic3() {
+    myPid.src = '/DOM/img/click-3.jpg';
+}
+*/
